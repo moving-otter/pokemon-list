854575f6ac4ca906027eb83d9a6cccee3859168e
otter <gusdn1460@nate.com>
2024-10-31 13:25:18 +0900

    feat(ui): Update tab tab head info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import React from "react";
+import Head from "next/head";
 import { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -8,6 +9,11 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>포켓몬 도감</title>
+        <link rel="icon" href="/favicon/monsterball-312x320.png" />
+      </Head>
+
       <Component {...pageProps} />
     </QueryClientProvider>
   );
