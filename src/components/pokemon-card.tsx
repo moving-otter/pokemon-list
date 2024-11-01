ed6e0ec373c89b3437f1bfbae49fd751b4ea6e13
otter <gusdn1460@nate.com>
2024-11-02 01:22:47 +0900

    style(ui): Update pokemon-card

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -18,16 +18,14 @@ export default function PokemonCard(props: PokemonCardProps) {
   return (
     <Link
       href={`/pokemon/${number}`}
-      className="block bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-200 relative flex flex-col" // Make the card a flex container
+      className="block bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-200 relative flex flex-col mx-1" // Reduce horizontal margin
     >
-      <div className="relative w-full aspect-1 overflow-hidden flex-grow p-1">
-        {' '}
-        {/* Flex-grow to take available space */}
+      <div className="relative w-full aspect-[3/2] overflow-hidden flex-grow p-1">
         {imageUrl ? (
           <img
             src={imageUrl}
             alt={name}
-            className="w-full h-full object-cover" // Keep the aspect ratio while covering the container
+            className="w-full h-full object-contain transform scale-85" // Further reduced scale to decrease image size
           />
         ) : (
           <div className="flex items-center justify-center w-full h-full bg-gray-100 text-gray-500">
@@ -35,26 +33,27 @@ export default function PokemonCard(props: PokemonCardProps) {
           </div>
         )}
       </div>
-      <div className="px-3 pb-2">
+      <div className="px-3 pb-3">
         {' '}
-        {/* Content below the image */}
+        {/* Reduced padding for the text section */}
         <div className="flex">
-          <div className="text-gray-600 text-sm font-semibold"># {number}</div>
+          <div className="text-gray-600 text-xs font-semibold"># {number}</div>{' '}
+          {/* Smaller font size */}
         </div>
         <div className="flex justify-between">
-          <h2 className="text-base font-bold capitalize">{name}</h2>
+          <h2 className="text-base font-bold capitalize">{name}</h2> {/* Adjusted font size */}
         </div>
         <div>
           <span className="text-xs font-medium">Height: {height / 10} m</span>
           <br />
           <span className="text-xs font-medium">Weight: {weight / 10} kg</span>
         </div>
-        <div className="mt-2 mb-2">
+        <div className="mt-1 mb-1">
           <div className="flex flex-wrap">
             {types.map((type) => (
               <span
                 key={type}
-                className={`inline-block text-xs font-semibold mr-2 px-2.5 py-0.5 rounded type-${type}`}
+                className={`inline-block text-xs font-semibold mr-1 px-1 py-0.5 rounded type-${type}`} // Smaller padding
               >
                 {type.charAt(0).toUpperCase() + type.slice(1)}
               </span>
