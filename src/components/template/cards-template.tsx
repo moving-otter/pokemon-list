bd0f2045bde7d9f6f0763f01befe09a14d2fff93
otter <gusdn1460@nate.com>
2024-11-05 23:03:21 +0900

    feat(ui): Update cards-template

--- src/components/template/cards-template.tsx
+++ src/components/template/cards-template.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card} from '@/components/organism';
 import {CardEmpty} from '@/components/molecule';
-import {useFinderStore} from '@/store/finder-store';
+// import {useFinderStore} from '@/store/finder-store';
 
 interface CardsTemplateProps {
   pokemonByIdsList: any;
@@ -10,16 +10,13 @@ interface CardsTemplateProps {
 export default function CardsTemplate(props: CardsTemplateProps) {
   // const sortOption = useFinderStore((state) => state.sortOption);
   const {pokemonByIdsList} = props;
-
-  const cardsListData = pokemonByIdsList;
-
-  const enableCondition = cardsListData?.length !== 0;
+  const hasCard = pokemonByIdsList?.length !== 0;
 
   return (
     <div data-testid="cards-template" className="flex-grow overflow-y-auto p-4">
-      {enableCondition ? (
+      {hasCard ? (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-2">
-          {cardsListData?.map((card: any) => (
+          {pokemonByIdsList?.map((card: any) => (
             <Card
               key={card.number}
               name={card.name}
