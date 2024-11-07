0e5802dc0fc633dd23a52a8e9f3409c18ce378a7
otter <gusdn1460@nate.com>
2024-11-07 21:10:13 +0900

    refactor: pagination 5 - connect initialListParams to Pagination

--- src/components/template/pagination.tsx
+++ src/components/template/pagination.tsx
@@ -1,4 +1,5 @@
 import {useRouter} from 'next/router';
+import {initialListParams} from '@/utils/constants';
 import {PokemonsListParam} from '@/services/pokemon/types';
 import {useEffect, useState} from 'react';
 import {Dropdown, Pagination as SemanticPagination} from 'semantic-ui-react';
@@ -13,21 +14,22 @@ interface PaginationProps {
 }
 
 export default function Pagination(props: PaginationProps) {
+  const _limit = initialListParams.limit; // limit 초기값
   const {totalCount, totalPages, listParams, triggerRerender, setListParams} = props;
   const router = useRouter();
   const [currentPage, setCurrentPage] = useState(1);
   const userSelectAll = listParams.limit === totalCount;
   const defaultLimitValue = router.query.limit ? Number(router.query.limit) : listParams.limit;
   const limitOptions = [
-    {key: '20', value: 20, text: '20'},
-    {key: '50', value: 50, text: '50'},
-    {key: '100', value: 100, text: '100'},
-    {key: totalCount, value: totalCount, text: 'All'},
+    {key: '1', value: _limit, text: _limit},
+    {key: '2', value: 50, text: '50'},
+    {key: '3', value: 100, text: '100'},
+    {key: '4', value: totalCount, text: 'All'},
   ];
 
   useEffect(() => {
     const page = Number(router.query.page) || 1;
-    const limit = Number(router.query.limit) || 20;
+    const limit = Number(router.query.limit) || _limit;
 
     setCurrentPage(page);
     setListParams({
@@ -49,7 +51,7 @@ export default function Pagination(props: PaginationProps) {
 
     router.push({
       pathname: router.pathname,
-      query: {...router.query, page, limit: listParams.limit || 20},
+      query: {...router.query, page, limit: listParams.limit || _limit},
     });
   };
 
